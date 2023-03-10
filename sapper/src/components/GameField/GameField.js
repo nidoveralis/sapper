08e27d5ed11ae185ed1764fcaea12525151f9d77
Nidoveralis <nidoveralis@gmail.com>
2023-03-10 22:19:53 +0400

    feat: create game field

--- sapper/src/components/GameField/GameField.js
+++ sapper/src/components/GameField/GameField.js
@@ -1,13 +1,27 @@
-import cell from '../../images/minesweeper-sprites_9TPZzv3.png';
+import React from 'react';
 import './GameField.css';
 import Cell from '../Cell/Cell';
 
-function GameField({countStart}) {
+function GameField({field,openCells,countStart,plantFlag,removeFlag, handelBlanckCell,open,gameOver}) {
 
+  const [bigBoom, setBigBoom] = React.useState(false);
+  const [bm, setBm] = React.useState(false);
+
+function a(item) {
+  countStart(item)
+}
+
+function openBombs() {
+  setBigBoom(true)
+}
+
+function opens() {
+  setBm(true);
+}
 
   return(
     <div className='field'>
-      { [...Array(256)].map((item, index) => <Cell  key={index} countStart={countStart} /> ) }
+      { field.map((item, index) => <Cell  key={index} countStart={a} plantFlag={plantFlag} removeFlag={removeFlag} openBombs={opens} item={item} handelBlanckCell={openBombs} open={open} field={field} bigBoom={bigBoom} openCells={openCells} />) }
     </div>
   )
 }
