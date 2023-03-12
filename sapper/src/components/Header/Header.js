d63cf2a661d239b7ab38e089dd7d309286220b1c
Nidoveralis <nidoveralis@gmail.com>
2023-03-12 21:29:29 +0400

    feat: stop time

--- sapper/src/components/Header/Header.js
+++ sapper/src/components/Header/Header.js
@@ -3,27 +3,41 @@ import './Header.css';
 import cell from '../../images/minesweeper-sprites_9TPZzv3.png';
 
 
-function Header({start, bombsCount,bigBoomm}) {
+function Header({start, bombsCount, loss}) {
   const [ time, setTime ] = React.useState(0);
   const [ startGame, setStartGame ] = React.useState(false);
+  const [smile, setSmile] = React.useState('-1px');
   
   const seconds = time % 10;
   const tens = parseInt(time / 10);
   const hundreds = parseInt(time / 100);
   
   function restartGame() {
+    t(0)
     setStartGame(false);
+    setSmile('-1px');
   };
+
+  function t(data) {
+    setTimeout(setTime, 1000, data);
+  }
 
   React.useEffect(() => {
     if (startGame) {
-      setTimeout(setTime, 1000, time + 1);
-    } 
+      t(time + 1)
+    }
   }, [ time, startGame]);
-
+  //console.log(start)
   React.useEffect(() => {
     setStartGame(start)
   },[start]);
+
+  React.useEffect(() => {
+    if(loss) {
+      setSmile('31px');
+      setStartGame(false);
+    }
+  },[loss]);
 
   return (
     <div className="header">
@@ -32,7 +46,7 @@ function Header({start, bombsCount,bigBoomm}) {
         <div className='count' style={{ backgroundImage: `url(${cell})`, backgroundPositionX: `${(13 - (14 * parseInt(bombsCount/10)))}px`}} />
         <div className='count' style={{ backgroundImage: `url(${cell})`, backgroundPositionX: `${(13 - (14 * (bombsCount % 10)))}px`}} />
       </div>
-      <div className='smile' style={{ backgroundImage: `url(${cell})`, backgroundPositionX: `-1px` }} onClick={restartGame}>
+      <div className='smile' style={{ backgroundImage: `url(${cell})`, backgroundPositionX: `${smile}` }} onClick={restartGame}>
         
       </div>
       <div className='second-count counts'>
